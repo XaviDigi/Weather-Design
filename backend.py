cbf90483bef368c0989c42d060bebdf3ce24a6ce
Xavier <Ismaelkf@gmail.com>
2024-02-21 08:22:01 +0900

    Day32:

--- backend.py
+++ backend.py
@@ -2,18 +2,14 @@ import requests
 
 API_KEY = "63e1fa7aab0a1b752382fa0c6e404d08"
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
